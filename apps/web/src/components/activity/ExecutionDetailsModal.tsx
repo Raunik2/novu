008baf2c0a656ca0fe2878c920b24718245c79ba
Pablo Fernández <pablo@novu.co>
2022-10-21 00:04:30 +0100

    feat(web): execution details list inside modal

--- apps/web/src/components/activity/ExecutionDetailsModal.tsx
+++ apps/web/src/components/activity/ExecutionDetailsModal.tsx
@@ -1,10 +1,70 @@
-import { Group, Modal, useMantineTheme } from '@mantine/core';
-import { colors, shadows, Title, Text } from '../../design-system';
+import { Grid, LoadingOverlay, Modal, useMantineTheme } from '@mantine/core';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+import { ExecutionDetailsSteps } from './ExecutionDetailsSteps';
+
+import { getActivityForNotification } from '../../api/activity';
+import { colors, shadows, Title, Text } from '../../design-system';
 import { GotAQuestionButton } from '../utils/GotAQuestionButton';
 
-export function ExecutionDetailsModal({ modalVisibility, onClose }: { modalVisibility: boolean; onClose: () => void }) {
+const LinkText = styled(Text)`
+  color: ${colors.B60};
+  font-size: 14px;
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ActionsWrapper = styled(LinkWrapper)`
+  margin: 0;
+`;
+
+const ExecutionDetailsFooter = ({ onClose, origin }) => {
+  const linkText = ` Back to ${origin}`;
+
+  return (
+    <Grid justify="center">
+      <Grid.Col span={2}>
+        <LinkWrapper>
+          <Link to={origin} onClick={onClose}>
+            <LinkText>{linkText}</LinkText>
+          </Link>
+        </LinkWrapper>
+      </Grid.Col>
+      <Grid.Col span={2} offset={8}>
+        <ActionsWrapper>
+          {/* TODO: Button has a margin top that's not possible to overload */}
+          <GotAQuestionButton mt={30} size="md" />
+        </ActionsWrapper>
+      </Grid.Col>
+    </Grid>
+  );
+};
+
+export function ExecutionDetailsModal({
+  notificationId,
+  modalVisibility,
+  origin,
+  onClose,
+}: {
+  notificationId: string;
+  modalVisibility: boolean;
+  onClose: () => void;
+  origin: string;
+}) {
   const theme = useMantineTheme();
+  const { data, isLoading, isFetching } = useQuery(['activity', 'notificationId'], function () {
+    if (notificationId) {
+      return getActivityForNotification(notificationId);
+    }
+  });
+
+  console.log('notificationId', notificationId, 'data', data);
 
   return (
     <Modal
@@ -14,6 +74,7 @@ export function ExecutionDetailsModal({ modalVisibility, onClose }: { modalVisib
       styles={{
         modal: {
           backgroundColor: theme.colorScheme === 'dark' ? colors.B15 : colors.white,
+          width: '60%',
         },
         body: {
           paddingTop: '5px',
@@ -29,12 +90,16 @@ export function ExecutionDetailsModal({ modalVisibility, onClose }: { modalVisib
       size="lg"
       onClose={onClose}
     >
-      <div>
-        <Text>TODO</Text>
-        <Group position="right">
-          <GotAQuestionButton mt={30} size="md" />
-        </Group>
-      </div>
+      <LoadingOverlay
+        visible={isLoading || isFetching}
+        overlayColor={theme.colorScheme === 'dark' ? colors.B30 : colors.B98}
+        loaderProps={{
+          color: colors.error,
+        }}
+        data-test-id="execution-details-modal-loading-overlay"
+      />
+      <ExecutionDetailsSteps steps={[]} />
+      <ExecutionDetailsFooter onClose={onClose} origin={origin} />
     </Modal>
   );
 }
